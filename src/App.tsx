25c2203e56c97d8c6046b55ce2b02ff4857c27b3
Viviane Mayra <vivi.mayra01@gmail.com>
2023-04-08 15:50:48 -0300

    Criando projeto e instalando chakra ui

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@chakra-ui/react"
+
+export function App() {
+  return (
+    <Box display="flex" flexDirection="column" margin="2rem 5rem">
+      <Text>Novo Projeto</Text>
+    </Box>
+  )
+}
