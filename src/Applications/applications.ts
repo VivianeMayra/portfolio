c0e7ec28e1873dda82d6ec8c076fecb605412b6f
Viviane Mayra <vivi.mayra01@gmail.com>
2023-04-17 20:21:53 -0300

    Criação do modal

--- src/Applications/applications.ts
+++ src/Applications/applications.ts
@@ -2,6 +2,7 @@ export const applications = [
   {
     id: 1,
     avatarUrl: "CoffeeDelivery.svg",
+    githubUrl: "https://github.com/VivianeMayra/coffee-delivery",
     name: " Coffe Delivery",
     tags: ["TypeScript", "HTML"],
     description:
@@ -10,41 +11,46 @@ export const applications = [
   {
     id: 2,
     avatarUrl: "DtMoney.svg",
-    name: " Coffe Delivery",
-    tags: ["TypeScript", "HTML"],
+    name: " DT Money",
+    githubUrl: "https://github.com/VivianeMayra/dt-money",
+    tags: ["TypeScript", "HTML", "Shell"],
     description:
-      "Fala rede! Hoje quero compartilhar com vocês mais um projeto desenvolvido com base no meu aprendizado com a stack JavaScript. A aplicação de hoje foi um e-commerce de cafés, no qual os clientes podem selecionar as bebibas desejadas, fazer a escolha da sua forma de pagamento e cadastrar em um formulário bem intuitivo e prático o endereço para a entrega do seu pedido!Apesar das poucas funcionalidades como a listagem de produtos (cafés) disponíveis para compra, aumentar ou remover a quantidade de itens no carrinho, exibir o valor total da soma das bebidas selecionadas e multiplicar seus valores... precisei relembrar conceitos importantíssimos desde ContextAPI no React até componentização  ",
+      "Fala rede! Queria compartilhar aplicação desenvolvida para ajudar outros usuários a terem um maior controle financeiro. Ela controla seus saques, transações e gastos!",
   },
   {
     id: 3,
     avatarUrl: "IgniteShop.svg",
-    name: " Coffe Delivery",
-    tags: ["TypeScript", "HTML"],
+    githubUrl: "https://github.com/VivianeMayra/ignite-shop",
+    name: " Ignite Shop",
+    tags: ["TypeScript", "JavaScript", "NextJs"],
     description:
-      "Fala rede! Hoje quero compartilhar com vocês mais um projeto desenvolvido com base no meu aprendizado com a stack JavaScript. A aplicação de hoje foi um e-commerce de cafés, no qual os clientes podem selecionar as bebibas desejadas, fazer a escolha da sua forma de pagamento e cadastrar em um formulário bem intuitivo e prático o endereço para a entrega do seu pedido!Apesar das poucas funcionalidades como a listagem de produtos (cafés) disponíveis para compra, aumentar ou remover a quantidade de itens no carrinho, exibir o valor total da soma das bebidas selecionadas e multiplicar seus valores... precisei relembrar conceitos importantíssimos desde ContextAPI no React até componentização  ",
+      "Fala Rede! Mais uma aplicação desenvolvida com base na stack JavaScript. Hoje me desafiei a aprender novas tecnologias e a escolhida foi NextJs, um dos frameworks mais famosos atualmente. O estilo da aplicação basicamente é um e-commerce de camisetas, na qual para sua criação implementei StitchesJs, passando pelos conceitos de SPA, SSR e SSG. E pra finalizar com chave de ouro, na parte de checkout ainda integrei com o Stripe, um software que oferece processamento de pagamentos e soluções comerciais.",
   },
   {
     id: 4,
     avatarUrl: "IgniteTime.svg",
-    name: " Coffe Delivery",
+    name: " Ignite Time",
+    githubUrl: "https://github.com/VivianeMayra/Ignite-Timer",
     tags: ["TypeScript", "HTML"],
     description:
-      "Fala rede! Hoje quero compartilhar com vocês mais um projeto desenvolvido com base no meu aprendizado com a stack JavaScript. A aplicação de hoje foi um e-commerce de cafés, no qual os clientes podem selecionar as bebibas desejadas, fazer a escolha da sua forma de pagamento e cadastrar em um formulário bem intuitivo e prático o endereço para a entrega do seu pedido!Apesar das poucas funcionalidades como a listagem de produtos (cafés) disponíveis para compra, aumentar ou remover a quantidade de itens no carrinho, exibir o valor total da soma das bebidas selecionadas e multiplicar seus valores... precisei relembrar conceitos importantíssimos desde ContextAPI no React até componentização  ",
+      "Dia de compartilhar mais uma aplicação desenvolvida com a stack javaScript 👩‍💻 dessa vez me desafiei a estruturar o projeto de forma diferente para por tudo que estou aprendendo em prática como, styled components, realizei também a criação de rotas com o React Router DOM, apliquei também fundamentos de contextos e Reducers. Basicamente o intuito da aplicação é auxiliar o usuário a ser mais organizado no cotidiano e poder cronometrar o tempo de suas tarefas, podendo definir suas tasks, a duração que deseja e consultar na página de histórico tudo que ele fez ao longo do seu dia por meio do status de tarefas entre concluída, em andamento e interrompida ",
   },
   {
     id: 5,
     avatarUrl: "Todo.svg",
-    name: " Coffe Delivery",
-    tags: ["TypeScript", "HTML"],
+    name: " Todo List",
+    githubUrl: "https://github.com/VivianeMayra/toDo-",
+    tags: ["JavaScript", "Css", "HTML"],
     description:
-      "Fala rede! Hoje quero compartilhar com vocês mais um projeto desenvolvido com base no meu aprendizado com a stack JavaScript. A aplicação de hoje foi um e-commerce de cafés, no qual os clientes podem selecionar as bebibas desejadas, fazer a escolha da sua forma de pagamento e cadastrar em um formulário bem intuitivo e prático o endereço para a entrega do seu pedido!Apesar das poucas funcionalidades como a listagem de produtos (cafés) disponíveis para compra, aumentar ou remover a quantidade de itens no carrinho, exibir o valor total da soma das bebidas selecionadas e multiplicar seus valores... precisei relembrar conceitos importantíssimos desde ContextAPI no React até componentização  ",
+      "Fala Rede!  Tirei a tarde desenvolvendo essa aplicação no estilo to-do list, que contém as seguintes funcionalidades: adicionar uma nova tarefa, marcar e desmarcar uma task como concluída, Remover um item da listagem e mostrar o progresso de conclusão das atividades. Usei os conceitos mais importantes do react e me aventurei também em aplicar os métodos de manipulação de arrays como map, filter ou até mesmo reduce",
   },
   {
     id: 6,
     avatarUrl: "IgniteFeed.svg",
-    name: " Coffe Delivery",
-    tags: ["TypeScript", "HTML"],
+    name: " Ignite Feed",
+    githubUrl: "https://github.com/VivianeMayra/IginiteFeed",
+    tags: ["JavaScript", "HTML", "CSS"],
     description:
-      "Fala rede! Hoje quero compartilhar com vocês mais um projeto desenvolvido com base no meu aprendizado com a stack JavaScript. A aplicação de hoje foi um e-commerce de cafés, no qual os clientes podem selecionar as bebibas desejadas, fazer a escolha da sua forma de pagamento e cadastrar em um formulário bem intuitivo e prático o endereço para a entrega do seu pedido!Apesar das poucas funcionalidades como a listagem de produtos (cafés) disponíveis para compra, aumentar ou remover a quantidade de itens no carrinho, exibir o valor total da soma das bebidas selecionadas e multiplicar seus valores... precisei relembrar conceitos importantíssimos desde ContextAPI no React até componentização  ",
+      "Queria compartilhar essa aplicação React que fiz nesse início de 2023 utilizando o Vite e aplicando alguns dos vários conceitos importantíssimos dessa biblioteca 😁 mesclei componentização, props, estados e imutabilidade por exemplo. Basicamente é um feed no qual desenvolvedores podem falar de seus projetos criados e receberem o feedback de outros devs! ",
   },
 ]
--- src/components/ProjectsCards/index.tsx
+++ src/components/ProjectsCards/index.tsx
@@ -1,4 +1,19 @@
-import { Box, Image } from "@chakra-ui/react"
+import {
+  Box,
+  Button,
+  Image,
+  Link,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Tag,
+  TagLabel,
+  useDisclosure,
+} from "@chakra-ui/react"
 
 export interface Project {
   id: number
@@ -6,16 +21,53 @@ export interface Project {
   name: string
   avatarUrl: string
   description: string
+  githubUrl: string
 }
 
-interface ProjectProps {
+export interface ProjectProps {
   project: Project
 }
 
 export function ProjectsCards({ project }: ProjectProps) {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   return (
-    <Box borderRadius="10px" height="200px">
+    <Button onClick={onOpen} key={project.id} height="250px" padding={4}>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>{project.name}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody display="flex" flexDirection="column">
+            {project.description}
+            <Box marginTop={2}>
+              {project.tags.map((tag) => (
+                <Tag
+                  padding={2}
+                  borderRadius="full"
+                  variant="outline"
+                  colorScheme="blue"
+                  marginLeft={2}
+                >
+                  <TagLabel key={`${project.id}${tag}`}>{tag}</TagLabel>
+                </Tag>
+              ))}
+            </Box>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">
+              <Link href={project.githubUrl} target="blanck">
+                {" "}
+                Link github
+              </Link>
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
       <Image src={`/applications/${project.avatarUrl}`} />
-    </Box>
+      <Box marginTop="10rem"></Box>
+    </Button>
   )
 }
